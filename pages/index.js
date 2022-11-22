06939e3356097c3f790828a8be41a8866f1c3a2e
wonny-won <jaake97@newbizstart.com>
2022-11-22 22:59:05 +0900

    2022-11-22:폴더구조 잡기

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+import UserMain from "../src/components/unit/user/home/userHome.contatiner"
+export default function Home() {
+  return <UserMain />
+}
---
+++ src/components/unit/user/home/userHome.contatiner.js
@@ -0,0 +1,7 @@
+export default function UserMain(){
+    return(
+        <>
+            유저 메인페이지
+        </>
+    )
+}
