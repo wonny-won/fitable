e4ef8fea98a58048a6c3bf69e407cb2b6a44c92e
wonny-won <jaake97@letsdingco.com>
2022-11-25 23:02:43 +0900

    헤더 배치 수정

--- src/components/commons/layout/header/header.presenter.tsx
+++ src/components/commons/layout/header/header.presenter.tsx
@@ -5,14 +5,16 @@ export default function HeaderUI(){
         <>
             <S.HeaderWrapper>
                 <div> Fit Your Life </div>
-                <ul>
-                    <li>다이어트 프로그램</li>
-                    <li>건강관리 프로그램</li>
-                    <li>맞춤 프로그램 진단</li>
-                    <li>보조제 마켓</li>
-                </ul>
-               <div> 검색 </div>
-               <div> 마이페이지 </div>
+                <S.MenuWrapper>
+                    <S.MenuLi>다이어트 프로그램</S.MenuLi>
+                    <S.MenuLi>건강관리 프로그램</S.MenuLi>
+                    <S.MenuLi>맞춤 프로그램 진단</S.MenuLi>
+                    <S.MenuLi>보조제 마켓</S.MenuLi>
+                </S.MenuWrapper>
+                <div>
+                    <div> 검색 </div>
+                    <div> 마이페이지 </div>
+                </div>
             </S.HeaderWrapper>
         </>
     )
--- src/components/commons/layout/header/header.style.ts
+++ src/components/commons/layout/header/header.style.ts
@@ -2,5 +2,12 @@ import styled from "@emotion/styled";
 
 export const HeaderWrapper = styled.div`
     display: flex;
-    
+    justify-content: space-between;
+`
+export const MenuWrapper = styled.ul`
+    display: flex;
+    margin : 0 20px;
+`
+export const MenuLi = styled.li`
+    margin : 0px 10px
 `
