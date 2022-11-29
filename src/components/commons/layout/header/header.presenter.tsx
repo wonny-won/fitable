5b679eba330dad971df30ae80d81a4a452c67051
wonny-won <jaake97@letsdingco.com>
2022-11-29 21:56:58 +0900

    2022-11-29 :유저  레이아웃 헤더 수정

--- src/components/commons/layout/header/header.presenter.tsx
+++ src/components/commons/layout/header/header.presenter.tsx
@@ -6,10 +6,9 @@ export default function HeaderUI(){
             <S.HeaderWrapper>
                 <div> Fit Your Life </div>
                 <S.MenuWrapper>
-                    <S.MenuLi>다이어트 프로그램</S.MenuLi>
-                    <S.MenuLi>건강관리 프로그램</S.MenuLi>
+                    <S.MenuLi>프로그램 상담</S.MenuLi>
                     <S.MenuLi>맞춤 프로그램 진단</S.MenuLi>
-                    <S.MenuLi>보조제 마켓</S.MenuLi>
+                    <S.MenuLi>스토어</S.MenuLi>
                 </S.MenuWrapper>
                 <div>
                     <div> 검색 </div>
