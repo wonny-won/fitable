a65f5df807fa5121d5bb105651f527f864a4ffb0
wonny-won <jaake97@newbizstart.com>
2022-12-05 18:15:01 +0900

    2022-12-5 : 카드 뒷편 생성

--- src/components/commons/atom/user/mainCard/mainCard.presenter.tsx
+++ src/components/commons/atom/user/mainCard/mainCard.presenter.tsx
@@ -4,16 +4,19 @@ import { MainCardProps } from './mainCard.types'
 export default function MainCardUI(props:MainCardProps){
     return(
         <>
-            <h1 style={{display: "none"}}> 다이어트 프로그램</h1>
+            <h1 style={{display: "none"}}>프로그램 카드</h1>
                 <S.CardWrap>
-                    <S.ProgramCard image={props.image}>
+                    <S.FrontProgramCard image={props.image} className="front">
                     <S.CardTitle>
                         {
                             props.image === '/dietprogram.svg' ? "Diet program" : "Healing program"
                         }
                     </S.CardTitle>
                     <S.Line />
-                    </S.ProgramCard>
+                    </S.FrontProgramCard>
+                    <S.BackProgramCard className="back">
+                        <button>뒷면!</button>
+                    </S.BackProgramCard>
                 </S.CardWrap>
         </>
     )
--- src/components/commons/atom/user/mainCard/mainCard.styled.ts
+++ src/components/commons/atom/user/mainCard/mainCard.styled.ts
@@ -1,15 +1,17 @@
 import styled from "@emotion/styled"
 
 interface ImageProps{
-    image : string ;
+    image? : string ;
 }
 
 export const CardWrap = styled.div`
     align-items: center;
     width: 400px;
     height: 450px;
+    /* 부모의 자식요소가 3차원의 애니메이션 효과를 가질때 300px의 거리에서 보는 원근감을 줌 */
+    perspective: 300px; 
 `
-export const ProgramCard = styled.div`
+export const FrontProgramCard = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -19,6 +21,8 @@ export const ProgramCard = styled.div`
     background-image: url(${(props:ImageProps) => props.image});
     background-size: cover;
     border-radius: 30px;
+    position: absolute;
+    transition: ls;
     &:hover{
         width: 410px;
         height:460px;
@@ -26,6 +30,26 @@ export const ProgramCard = styled.div`
     }
     box-shadow: 2px 6px 7px rgba(0,0,0,0.25);
 `
+export const BackProgramCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 450px;
+    background-image: url(${(props:ImageProps) => props.image});
+    background-size: cover;
+    border-radius: 30px;
+    backface-visibility: hidden;
+    transition: ls;
+    &:hover{
+        width: 410px;
+        height:460px;
+        cursor: pointer;
+    }
+    box-shadow: 2px 6px 7px rgba(0,0,0,0.25);
+`
+
 export const CardTitle = styled.div`
     font-size: 36px;
     font-weight: 400;
