62d946e2f532626d156c5a14fd4553e3111a2bf3
wonny-won <jaake97@letsdingco.com>
2022-12-09 17:58:46 +0900

    2022-12-9 : 카드 뒤집기 유지 성공

--- src/components/commons/atom/user/mainCard/mainCard.styled.ts
+++ src/components/commons/atom/user/mainCard/mainCard.styled.ts
@@ -22,27 +22,30 @@ export const FrontProgramCard = styled.div`
     border-radius: 30px;
     transition: .8s;
     position: absolute;
-    /* transform: rotateY(0deg); */
+    transform: rotateY(0deg);
     backface-visibility: hidden;
-    &:active{
+    &:focus-within{
         cursor: pointer;
         transform: rotateY(180deg);
     }
     box-shadow: 2px 6px 7px rgba(0,0,0,0.25);
 `
 export const BackProgramCard = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 400px;
     height: 450px;
     border-radius: 30px;
     transition: .8s;
     backface-visibility: hidden;
-    transform: rotateY(-180deg);
-    &:hover{
+    border: 1px solid black;
+    transform: rotateY(0deg);
+    &:focus-within{
         cursor: pointer;
-        transform: rotateY(0deg);
+        transform: rotateY(180deg);
     }
 `
-
 export const CardTitle = styled.div`
     font-size: 36px;
     font-weight: 400;
