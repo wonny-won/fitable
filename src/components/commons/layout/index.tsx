7f8337eaa2ca596f8a985f1bb89ac4cdc368735d
wonny-won <jaake97@letsdingco.com>
2022-12-20 17:58:09 +0900

    2022-12-20:hidden banner

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,15 +1,19 @@
 import Footer from "./footer/footer.container";
 import Header from "./header/header.container"
 import Banner from "./banner/banner.presenter";
+import { useRouter } from "next/router";
 interface LayoutProps{
     children: JSX.Element;
 }
 export default function Layout(props:LayoutProps){
+    const router = useRouter()
+    const HIDDEN_HEADER = ['/review/new']
+    const isHiddenHeader = HIDDEN_HEADER.includes(router.asPath)
     return(
         <>
             <Header />
-            <Banner />
-             <div>{props.children}</div>
+            {!isHiddenHeader && <Banner />}
+             <div style={{marginTop:"130px"}}>{props.children}</div>
             <Footer />
         </>
     )
