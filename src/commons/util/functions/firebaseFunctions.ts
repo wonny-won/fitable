b96633652716827824fd18ada79427f58606b942
wonny-won <jaake97@letsdingco.com>
2022-12-27 18:05:06 +0900

    2022-12-27:이미지 업로드-파이어베이스

--- src/commons/util/functions/firebaseFunctions.ts
+++ src/commons/util/functions/firebaseFunctions.ts
@@ -1,5 +1,6 @@
-import { DB } from "../../../../pages/_app";
+import { DB, FirebaseApp } from "../../../../pages/_app";
 import { collection,addDoc } from "firebase/firestore";
+import { getStorage,ref } from 'firebase/storage'
 
 interface FirebaseParams {
     colletionName: string;
@@ -11,4 +12,10 @@ export const addDocs = async ({colletionName, data} :FirebaseParams) => {
     const result = await addDoc(collection(DB,colletionName),data)
     alert("등록이 완료되었습니다.")
     return result.id
+}
+
+// 스토리지에 이미지 업로드 함수
+export const UploadImage = (ImageUrl:string)=>{
+    const storage = getStorage()
+    ref(storage,`newReview/${ImageUrl}`)
 }
