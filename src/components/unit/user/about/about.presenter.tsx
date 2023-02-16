d6bae08d9f1c6673b83922eec1229f3621e9ca07
wonny-won <jaake97@letsdingco.com>
2023-02-16 15:42:28 +0900

    2023-02-16: 결제 연결결

--- src/components/unit/user/about/about.presenter.tsx
+++ src/components/unit/user/about/about.presenter.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import FitableLogo from "../../../commons/atom/user/logo/logo.presenter"
 import * as S from './about.style'
+import { onClickPayment } from "../../../../commons/util/functions/payments"
 
 export default function AboutUI(){
     return(
@@ -10,6 +11,11 @@ export default function AboutUI(){
             <meta charSet="UTF-8" />
             <meta name="description" content="자기관리도 프리미엄의 시대! 내게 핏한 관리를 받아보세요." />
             <meta name="veiwport " content="width=device-width, initial-scale=1.0" />
+            {/* {/* 결제 - jQuery */}
+            <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" ></script>
+            {/* 결제 - iamport.payment.js */}
+            <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script> 
+
         </Head>
         <S.Container>
             <FitableLogo fontSize="40px" />
@@ -30,6 +36,7 @@ export default function AboutUI(){
                     <div>
                         고객님의 목표와 이유를 기반으로 프로그램 진행
                     </div>
+                    <div onClick={onClickPayment}>프로그램 신청하기</div>
                </S.ProgramWrap>
                <S.Img src="/dietprogram.jpg" alt="dietprogram"/>
             </S.DietSection>
@@ -41,6 +48,7 @@ export default function AboutUI(){
                     <div>
                         고객님의 현재 체형과 상태를 기반으로 프로그램 진행
                     </div>
+                    <div onClick={onClickPayment}>프로그램 신청하기</div>
                 </S.ProgramWrap>
             </S.HealingSection>
 
