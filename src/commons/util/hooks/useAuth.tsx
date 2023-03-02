7a52010809e3bbef0277e7c48ebf9549ab5d7ad1
wonny-won <jaake97@newbizstart.com>
2023-03-02 19:30:39 +0900

    2023-03-02: useAuth 완성

--- src/commons/util/hooks/useAuth.tsx
+++ src/commons/util/hooks/useAuth.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react"
 import { useIsLogInUser } from "./signUpIn"
-import { useRoutingPageHooks } from "./routing"
+// import { useRoutingPageHooks } from "./routing"
+import { useRouter } from "next/router"
 
 export default function UseAuth(){
+    const router = useRouter()
     const isLogin = useIsLogInUser()
     console.log(isLogin)
     useEffect(()=>{
         if(isLogin === false){
             alert("로그인이 필요한 서비스 입니다.")
+            router.push('/joinus')
         }   
-        useRoutingPageHooks()('/joinus') 
-
     },[])
 }
