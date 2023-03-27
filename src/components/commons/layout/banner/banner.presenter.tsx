bcbfb350f9c29a37a5e456d9ecaa10ed0ea7dd08
wonny-won <jaake97@letsdingco.com>
2023-03-27 19:34:37 +0900

    2023-03-27: 배너 변경

--- src/components/commons/layout/banner/banner.presenter.tsx
+++ src/components/commons/layout/banner/banner.presenter.tsx
@@ -6,9 +6,9 @@ export default function Banner (){
         <>
             <h1 style={{display:"none"}}>fitable 소개</h1>
             <S.TextWrap>
-                <S.MainText>당신의 몸을 위한 <S.TextSpan>최선의 선택</S.TextSpan></S.MainText>
+                <S.MainText>커피한잔의 여유 <S.TextSpan>최선의 선택</S.TextSpan></S.MainText>
                 <FitableLogo fontSize={"35px"}/>
-                <div>건강관리도 프리미엄의 시대, 집에서 <S.TextSpan>내게 꼭 맞는 진짜 관리</S.TextSpan>를 받아보세요</div>
+                <div>나의 수월한 이직을 위한 <S.TextSpan>포트폴리오 피드백</S.TextSpan>을 받아보세요</div>
             </S.TextWrap>
 
         </>
