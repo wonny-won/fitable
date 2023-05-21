ca3a46af6f12f245709d00ffb57613bd838928c6
wonny-won <jaake97@newbizstart.com>
2023-05-22 02:19:16 +0900

    23-05-22: review/new useBranchAuthority 에러 발견, 고치는 중

--- src/components/unit/user/review/new/reviewNewFn/authority.ts
+++ src/components/unit/user/review/new/reviewNewFn/authority.ts
@@ -1,17 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRoutingPageHooks } from "../../../../../../commons/util/hooks/routing";
 
 interface Param {
-    program:string|string[]|undefined;
+    program:any;
     isEdit: boolean;
 }
 
 export const useBranchAuthority = ({program,isEdit}:Param)=>{
+    // const [applyProgram,setapplyProgram] = useState()
     const routerHooks = useRoutingPageHooks()
-    useEffect(()=>{
+    console.log(program)
+    setTimeout((program:any)=>{
+        console.log('setTimeout',program)
         if(!program && !isEdit){
+            console.log('if',program)
             alert('잘못된 접근입니다.')
             routerHooks('/mypage')()
-        } 
-    },[])
+        }     
+    },5000,program,isEdit)
+    // useEffect(()=>{
+    //     setapplyProgram(program)    
+    //     if(!applyProgram && !isEdit){
+    //         console.log('if',applyProgram)
+    //         alert('잘못된 접근입니다.')
+    //         routerHooks('/mypage')()
+    //     }      
+    // },[])
 }
